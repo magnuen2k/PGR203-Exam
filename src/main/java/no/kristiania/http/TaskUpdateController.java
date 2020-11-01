107b0c4c9a780a357b375a36d4938175afffaa9a
smeepz <stianlw@gmail.com>
2020-11-01 16:15:31 +0100

    Tried to implement TaskUpdateController, missing update from ProductDao.

--- src/main/java/no/kristiania/http/TaskUpdateController.java
+++ src/main/java/no/kristiania/http/TaskUpdateController.java
@@ -1,7 +1,7 @@
 package no.kristiania.http;
 
+import no.kristiania.db.Task;
 import no.kristiania.db.TaskDao;
-
 import java.io.IOException;
 import java.net.Socket;
 import java.sql.SQLException;
@@ -16,6 +16,21 @@ public class TaskUpdateController implements HttpController{
 
     @Override
     public void handle(HttpMessage request, Socket clientSocket) throws SQLException, IOException {
+        HttpMessage response = handle(request);
+        response.write(clientSocket);
+    }
 
+    private HttpMessage handle(HttpMessage request) throws SQLException {
+        QueryString responseParameter = new QueryString(request.getBody());
+
+        Long taskId = Long.valueOf(responseParameter.getParameter("taskId"));
+        Integer projectId = Integer.valueOf(responseParameter.getParameter("projectId"));
+        Task task = taskDao.retrieve(taskId);
+        taskDao.update(task); //Johannes has the update on line 37 in ProductDao
+
+        HttpMessage redirect = new HttpMessage();
+        redirect.getStartLine("HTTP/1.1 302 Redirect";
+        redirect.getHeaders().put("Location", "http://localhost:8080/idenx.html");
+        return redirect;
     }
 }
